8fe4c98ab253d787c5d69ac462f3b941cf00bddf
tian <istiancz@gmail.com>
2024-02-22 11:53:28 +0800

    feat: add loading state

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,15 +1,12 @@
-import '@pages/popup/Popup.css';
-import useStorage from '@src/shared/hooks/useStorage';
-import exampleThemeStorage from '@src/shared/storages/exampleThemeStorage';
 import withSuspense from '@src/shared/hoc/withSuspense';
 import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
 import { useState, useEffect, useCallback } from 'react';
 import { host } from '../content/ui/app';
 
 const Popup = () => {
-  const theme = useStorage(exampleThemeStorage);
   const [id, setId] = useState('');
   const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   async function getCurrentTab() {
     const queryOptions = { active: true, currentWindow: true };
@@ -25,12 +22,16 @@ const Popup = () => {
   const getList = useCallback(async () => {
     const id = await getCurrentTab();
     if (!id) return;
+    setLoading(true);
     const res = await fetch(`${host}/youtube/api?id=${id}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       },
-    }).then(res => res.json());
+    })
+      .then(res => res.json())
+      .catch(err => console.log('err', err));
+    setLoading(false);
     console.log('rrrrr', res);
     setList(res.data);
   }, []);
@@ -39,28 +40,23 @@ const Popup = () => {
     getList();
   }, [getList]);
 
+  if (loading) return <div className="h-screen flex justify-center items-center text-2xl font-medium">Loading...</div>;
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor: theme === 'light' ? '#fff' : '#000',
-      }}>
+    <div className="text-center">
       {id ? (
-        <main>
-          <p>video id: {id}</p>
-          <button className="border rounded px-2 cursor-pointer text-lg" onClick={getList}>
-            update
+        <main className="pl-4 pb-4 pr-1">
+          <button onClick={getList} className="text-lg font-medium text-left">
+            total count: {list?.length}
+            <span className="text-sm text-gray-500 ml-4">{id}</span>
           </button>
-          <div>total count: {list?.length}</div>
-          <ul>
+          <ol className="list-disc text-base">
             {list?.map((comment, index) => (
-              <li key={index}>
-                <div>time:{comment.time}s</div>
-                <div>{comment.text}</div>
-                ----
+              <li className="text-left" key={index}>
+                <span className="text-blue-600 mr-2">{comment.time}</span>
+                {comment.text}
               </li>
             ))}
-          </ul>
+          </ol>
         </main>
       ) : (
         <main className="text-2xl font-medium">not a youtube video</main>

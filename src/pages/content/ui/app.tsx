bf01aeac3f958fe6d5c37ed50ff5e646a559dcdc
tian <istiancz@gmail.com>
2024-02-23 14:29:35 +0800

    feat: load live danmakus

--- src/pages/content/ui/app.tsx
+++ src/pages/content/ui/app.tsx
@@ -1,43 +1,65 @@
-import danmakuStorage, { YT_Response } from '@root/src/shared/storages/danmakuStarage';
 import { pickRandomColor } from '@root/src/utils/consts';
 import Danmaku from 'danmaku';
 import { useEffect, useRef, useCallback } from 'react';
+import { checkIsLive, getLiveChats, getComments } from './requests';
 
-export const host = 'https://danmaku-backend.vercel.app';
+interface Comment {
+  text?: string;
+  /**
+   * @default rtl
+   */
+  mode?: 'ltr' | 'rtl' | 'top' | 'bottom';
+  /**
+   * Specified in seconds. Not required in live mode.
+   * @default media?.currentTime
+   */
+  time?: number;
+  style?: Partial<CSSStyleDeclaration> | CanvasRenderingContext2D;
+  /**
+   * A custom render to draw comment.
+   * When it exist, `text` and `style` will be ignored.
+   */
+  render?(): HTMLElement | HTMLCanvasElement;
+}
+
+const isDev = true;
+export const host = isDev ? 'http://localhost:3000' : 'https://danmaku-backend.vercel.app';
+let timer: NodeJS.Timeout;
 export default function App() {
   const d = useRef(null);
   const currentId = useRef(null);
 
-  const loadRemote = async (id?: string) => {
-    let finalId = id;
-    if (!id) {
-      const search = window.location.search;
-      if (!search) throw new Error('No search');
-      finalId = window.location.search.replace('?v=', '');
-    }
-    const res = await fetch(`${host}/youtube/api?id=${finalId}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
+  const emitLiveComments = useCallback(async (channelId: string, pageToken?: string) => {
+    const { pollingIntervalMillis, items, nextPageToken } = await getLiveChats({ channelId, pageToken });
+    const comments = items.map(l => ({
+      text: l.text,
+      style: {
+        ...style,
+        color: pickRandomColor(),
       },
-    }).then(res => res.json());
-    console.log('rrrrr', res);
-    danmakuStorage.set(res.data);
-    return res.data as YT_Response;
-  };
-  const init = useCallback(async (id?: string) => {
-    d.current && d.current.destroy();
-    const list = await loadRemote(id);
-    const video = document.getElementsByTagName('video')[0];
-    console.log('init video', video);
-    if (!video)
-      return setTimeout(() => {
-        init(id);
-      }, 1000);
+    }));
+    return { pollingIntervalMillis, nextPageToken, comments };
+  }, []);
 
-    const container = video.parentNode as HTMLElement;
-    container.style.height = '100%';
-    console.log('damnaku loaded', video);
+  const initLiveChats = useCallback(
+    async (channelId: string, pageToken?: string) => {
+      console.log('initLiveChats', channelId, pageToken);
+      const { pollingIntervalMillis, nextPageToken, comments } = await emitLiveComments(channelId, pageToken);
+      if (d.current) {
+        comments.forEach(comment => d.current.emit(comment));
+      } else {
+        initDanmaku([]);
+      }
+      timer = setTimeout(() => {
+        initLiveChats(channelId, nextPageToken);
+      }, pollingIntervalMillis);
+    },
+    [emitLiveComments],
+  );
+
+  const initComments = useCallback(async (id?: string) => {
+    console.log('initComments', id);
+    const list = await getComments(id);
     const comments = list.map(l => ({
       ...l,
       style: {
@@ -45,6 +67,22 @@ export default function App() {
         color: pickRandomColor(),
       },
     }));
+    const video = document.getElementsByTagName('video')[0];
+    console.log('init video', video);
+    if (!video)
+      return setTimeout(() => {
+        initComments(id);
+      }, 1000);
+
+    initDanmaku(comments);
+  }, []);
+
+  const initDanmaku = (comments: Comment[]) => {
+    d.current && d.current.destroy();
+    const video = document.getElementsByTagName('video')[0];
+    const container = video.parentNode as HTMLElement;
+    container.style.height = '100%';
+    console.log('damnaku loaded', video);
     const danmaku = new Danmaku({
       // 必填。用于显示弹幕的「舞台」会被添加到该容器中。
       container,
@@ -63,10 +101,10 @@ export default function App() {
       speed: 144,
     });
     d.current = danmaku;
-  }, []);
+  };
   const emit = async () => {
     if (!d.current) {
-      await init();
+      await initComments();
     }
     const comment = {
       text: 'bla blaaaaaaa',
@@ -76,18 +114,22 @@ export default function App() {
   };
 
   useEffect(() => {
-    chrome.runtime.onMessage.addListener(function (message) {
+    chrome.runtime.onMessage.addListener(async function (message) {
       const id = message.id;
       if (!id) return;
       if (currentId.current === id) return;
       currentId.current = id;
 
       console.log('new video id:', id);
-      init(id);
+      timer && clearTimeout(timer);
+      const liveChannelId = await checkIsLive(id);
+      console.log('liveChannelId', liveChannelId);
+      if (liveChannelId) return initLiveChats(liveChannelId);
+      initComments(id);
     });
-  }, [init]);
+  }, [initComments, initLiveChats]);
 
-  return null;
+  // return null;
   return (
     <div className="fixed top-0 right-0 h-20 z-[9999] bg-gray-200/20 flex text-xl gap-4">
       <button className="px-4 py-2 rounded-full" onClick={emit}>
---
+++ src/pages/content/ui/requests.ts
@@ -0,0 +1,60 @@
+import danmakuStorage, { YT_Response } from '@root/src/shared/storages/danmakuStarage';
+import { host } from './app';
+
+const getId = (id?: string) => {
+  let finalId = id;
+  if (!id) {
+    const search = window.location.search;
+    if (!search) throw new Error('No search');
+    finalId = window.location.search.replace('?v=', '');
+  }
+
+  return finalId;
+};
+export const getComments = async (id?: string) => {
+  const finalId = getId(id);
+  const res = await fetch(`${host}/youtube/api?id=${finalId}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then(res => res.json());
+  console.log('rrrrr', res);
+  danmakuStorage.set(res.data);
+  return res.data as YT_Response;
+};
+
+export const checkIsLive = async (id?: string) => {
+  const finalId = getId(id);
+  const res = await fetch(`${host}/youtube/checkLive?id=${finalId}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then(res => res.json());
+  const channelId = res.channelId;
+  return channelId;
+};
+
+export const getLiveChats = async ({ channelId, pageToken }: { channelId: string; pageToken?: string }) => {
+  const pageTokenParam = pageToken ? `&pageToken=${pageToken}` : '';
+  const res = await fetch(`${host}/youtube/live?channelId=${channelId}${pageTokenParam}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then(res => res.json());
+  return res.data as YT_LIVE_RES;
+};
+
+interface YT_LIVE_RES {
+  pollingIntervalMillis: number | null | undefined;
+  nextPageToken: string | null | undefined;
+  items: {
+    text: string | null | undefined;
+    author: {
+      displayName: string;
+    };
+  }[];
+  channelId: string;
+}

48dbf834089857868d0383b98cacf1d747b76725
tian <istiancz@gmail.com>
2024-03-04 15:00:18 +0800

    feat: style update

--- twind.config.ts
+++ twind.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from '@twind/core';
-import presetTailwind from '@twind/preset-tailwind';
+import presetTailwind from '@twind/preset-tailwind/base';
 import presetAutoprefix from '@twind/preset-autoprefix';
+// Selectively import colors
+import * as colors from '@twind/preset-tailwind/colors';
 
 export default defineConfig({
-  presets: [presetAutoprefix(), presetTailwind()],
+  presets: [
+    presetAutoprefix(),
+    presetTailwind({
+      colors: {
+        ...colors,
+        primary: '#FF6666',
+      },
+    }),
+  ],
 });

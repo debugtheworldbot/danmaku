0ea7464ae3d5cb1b3da09c5f8dd8d88f0b4008aa
tian <istiancz@gmail.com>
2024-03-10 20:56:21 +0800

    style: update

--- src/pages/options/Options.tsx
+++ src/pages/options/Options.tsx
@@ -4,22 +4,19 @@ import { Logo } from './components/Logo';
 
 const Checkbox = ({ checked }: { checked?: boolean }) => {
   return (
-    <svg
-      className="w-5 h-5 me-2 text-green-500 flex-shrink-0"
-      style={{
-        color: checked ? '#4AB94B' : 'gray',
-      }}
-      aria-hidden="true"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="currentColor"
-      viewBox="0 0 20 20">
-      <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M10 20C15.5228 20 20 15.5228 20 10C20 4.47715 15.5228 0 10 0C4.47715 0 0 4.47715 0 10C0 15.5228 4.47715 20 10 20ZM4.70696 10.7781C4.31643 10.3876 4.31643 9.75445 4.70696 9.36392C5.09748 8.9734 5.73065 8.9734 6.12117 9.36392L8.9496 12.1923L13.8993 7.2426C14.2899 6.85208 14.923 6.85208 15.3136 7.2426C15.7041 7.63312 15.7041 8.26629 15.3136 8.65681L8.9496 15.0208L4.70696 10.7781Z"
+        fill={checked ? '#4AB94B' : '#052333'}
+      />
     </svg>
   );
 };
 
 const Kbd = (props: { text: string }) => (
-  <kbd className="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-[#F5F2F2] border border-gray-300 rounded-lg">
+  <kbd className="px-3 py-2 text-sm font-semibold text-gray-800 text-black bg-[#F5F2F2] border-[#EBE8E8] border rounded-lg">
     {props.text}
   </kbd>
 );
@@ -27,7 +24,7 @@ const Kbd = (props: { text: string }) => (
 const Options: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="flex justify-between items-center p-3 w-screen">
+      <header className="flex justify-between items-center py-3 px-12 w-screen">
         <Logo />
         <a
           className="rounded-full bg-primary py-1 px-4 text-white font-medium text-base"
@@ -37,7 +34,7 @@ const Options: React.FC = () => {
           Feedback
         </a>
       </header>
-      <div className="flex-1 flex bg-[#FCFAF0] p-8 gap-8">
+      <div className="flex-1 flex bg-[#FCFAF0] p-12 gap-8">
         <aside className="self-center relative">
           <Bg />
 
@@ -45,47 +42,47 @@ const Options: React.FC = () => {
             <SideAnim />
           </div>
         </aside>
-        <main className="flex-1 flex flex-col bg-white rounded-xl px-6 py-4">
-          <h2 className="mb-4 text-2xl font-semibold text-gray-900">Shortcuts</h2>
+        <main className="flex-1 flex flex-col bg-white rounded-xl p-12">
+          <h2 className="mb-8 text-2xl font-semibold text-gray-900 leading-4">Shortcuts</h2>
 
           <div className="relative overflow-x-auto">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500">
-              <thead className="text-sm text-gray-700 uppercase bg-[#F5F2F2] rounded-full">
+              <thead className="text-lg text-gray-700 bg-[#F5F2F2] rounded-full">
                 <tr>
-                  <th scope="col" className="px-6 py-3 rounded-l-lg">
+                  <th scope="col" className="px-4 py-2 rounded-l-lg font-medium">
                     Key
                   </th>
-                  <th scope="col" className="px-6 py-3 rounded-r-lg">
+                  <th scope="col" className="px-4 py-2 rounded-r-lg font-medium">
                     Description
                   </th>
                 </tr>
               </thead>
-              <tbody>
+              <tbody className="text-base text-[#666666]">
                 <tr className="bg-white border-b">
-                  <th scope="row" className="px-2 py-6 font-medium text-gray-500 whitespace-nowrap">
+                  <th scope="row" className="py-6 font-medium whitespace-nowrap">
                     <Kbd text="Enter" />
                     <span className="mx-2">or</span>
                     <Kbd text="return" />
                   </th>
-                  <td className="px-6 py-4">Open danmaku send dashboard</td>
+                  <td className="px-4 py-4">Open danmaku send dashboard</td>
                 </tr>
                 <tr className="bg-white border-b">
-                  <th scope="row" className="px-2 py-4 font-medium text-gray-500 whitespace-nowrap">
+                  <th scope="row" className="py-6 font-medium whitespace-nowrap">
                     <Kbd text="Ctrl + q" />
                     <span className="mx-2">or</span>
                     <Kbd text="Esc" />
                   </th>
-                  <td className="px-6 py-4 leading-8">
+                  <td className="px-4 py-4 leading-6">
                     <div>Close danmaku send dashboard</div>
-                    suggest use <Kbd text="Ctrl + q" /> , beacuse <Kbd text="Esc" /> will quit fullscreen mode :)
+                    suggest use <span className="font-bold">Ctrl + q</span>, beacuse Esc will quit fullscreen mode :)
                   </td>
                 </tr>
               </tbody>
             </table>
           </div>
 
-          <h2 className="mt-8 mb-4 text-2xl font-semibold text-gray-900">Support operations</h2>
-          <ul className="space-y-3 text-lg list-inside">
+          <h2 className="mt-12 mb-6 text-2xl font-semibold text-gray-900">Support operations</h2>
+          <ul className="space-y-2 text-lg list-inside">
             <li className="flex items-center gap-2">
               <Checkbox checked />
               Show comments with time tag as danmaku

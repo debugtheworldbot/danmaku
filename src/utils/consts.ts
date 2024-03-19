7a60b55e9f0c19a8e9eaa0ff47641f3c5345a071
tian <istiancz@gmail.com>
2024-03-19 17:08:51 +0800

    feat: update api domain

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,4 +1,4 @@
 const isDev = false;
-export const host = isDev ? 'http://localhost:3000' : 'https://danmaku-backend.vercel.app';
+export const host = isDev ? 'http://localhost:3000' : 'https://danmucat.com';
 export const COLOR_LIST = ['#FFFF00', '#FFAA02', '#FE0302', '#FFFFFF', '#89D5FF', '#00CD00', '#9D7CD8'];
 export const pickRandomColor = () => COLOR_LIST[Math.floor(Math.random() * COLOR_LIST.length)];
